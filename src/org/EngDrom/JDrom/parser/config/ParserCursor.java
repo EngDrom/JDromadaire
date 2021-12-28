e8adb41306632fd8d45a7961bc7165d1d3f9b402
MrThimote <mrthimote@gmail.com>
2021-12-28 02:56:03 +0100

    main: parser: added parser cursor The idea of the class ParserCursor is to represent where we are currently parsing, and where to go back if something fails to try another of the grammar branch

---
+++ src/org/EngDrom/JDrom/parser/config/ParserCursor.java
@@ -0,0 +1,64 @@
+package org.EngDrom.JDrom.parser.config;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import org.EngDrom.JDrom.lexer.Token;
+import org.EngDrom.JDrom.types.Node;
+
+public class ParserCursor {
+
+	public int tok_idx = 0;
+	private LinkedList<Integer> saves = new LinkedList<Integer>();
+	private ArrayList<Token> tokens;
+	private LinkedList<Object> arguments = new LinkedList<Object>();
+	private LinkedList<Integer> args_saves = new LinkedList<Integer>();
+	
+	public Object[] args() {
+		return arguments.toArray();
+	}
+	
+	public ParserCursor (ArrayList<Token> types) {
+		this.tokens = types;
+	}
+	
+	public void save() {
+		saves.add(this.tok_idx);
+		args_saves.add(arguments.size());
+	}
+	
+	public void free(boolean found) {
+		if (!found)
+			this.restore();
+		else {
+			int idx = this.saves.removeLast();
+			int arg_save = this.args_saves.removeLast();
+		}
+	}
+
+	public Token get_cur_token() {
+		return this.tokens.get(this.tok_idx);
+	}
+
+	public void restore() {
+		if (this.saves.size() == 0) {
+			tok_idx = 0;
+		} else {
+			tok_idx = this.saves.getLast();
+		}
+		
+		if (this.arguments.size() == 0) {
+			this.arguments.clear();
+		} else {
+			int rst = this.args_saves.getLast();
+			while (rst < this.arguments.size()) {
+				this.arguments.removeLast();
+			}
+		}
+	}
+	
+	public void addArgument(Object obj) {
+		this.arguments.add(obj);
+	}
+	
+}
